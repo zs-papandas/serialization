7747d26ddba4be39af7c7a603135b2f57bcf24b5
Papan Das <papan.das@zs.com>
2019-01-24 15:56:51 +0530

    product add account

--- models/product.go
+++ models/product.go
@@ -6,7 +6,7 @@ package models
 type Product struct {
 	SerialId       	string 			`json:"serialid"`
 	Created        	string 			`json:"created"`
-	Creator    		string			`json:"creator"`
+	Owner    		string			`json:"owner"`
 	Name           	string 			`json:"name"`
 	Expire          string 			`json:"expire"`
 	GTIN         	string 			`json:"gtin"`
