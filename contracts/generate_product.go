8791542fdd58d7a3faa459e5ebf88a77b1bc6ebe
Papan Das <papan.das@zs.com>
2019-01-29 11:15:31 +0530

    Auto Gen Product

--- contracts/generate_product.go
+++ contracts/generate_product.go
@@ -64,7 +64,7 @@ func (ac *GenerateProductContract) CreateProduct(APIstub shim.ChaincodeStubInter
 	}*/
 
 	
-	identity = args[0]
+	/*identity = args[0]
 	pname = args[1]
 	expired = args[2]
 	gtin = args[3]
@@ -77,7 +77,22 @@ func (ac *GenerateProductContract) CreateProduct(APIstub shim.ChaincodeStubInter
 	totalPallet, _ = strconv.Atoi(args[7])
     totalBox, _ = strconv.Atoi(args[8])
 	totalPacket, _ = strconv.Atoi(args[9])
-	totalItem, _ = strconv.Atoi(args[10])
+	totalItem, _ = strconv.Atoi(args[10])*/
+
+	identity = "a"
+	pname ="Product Name"
+	expired = "Expired"
+	gtin = "GTIN-23432"
+	lotnum = "LOTNUM"
+	status = "CREATED"
+	amt = "10000"
+	myStr = "10"
+	productType = "pallet"
+
+	totalPallet = 2
+    totalBox = 2
+	totalPacket = 2
+	totalItem = 2
 
 	
 
